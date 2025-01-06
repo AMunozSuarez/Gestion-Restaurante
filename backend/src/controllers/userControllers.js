2e5fce7d20a1c954ad473f5817ed5d4e635d86b8
alexG4M3R <alex.372003@gmail.com>
2025-01-06 20:01:36 -0300

    update user

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,21 +1,53 @@
+const userModel = require("../models/userModel");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 // GET USER INFO
 const getUserController = async (req, res) => {
-    res.status(200).send("todo bien");
-    // try {
-    //     const user = await userModel.findById(req.user.id).select('-password');
-    //     if (!user) {
-    //         return res.status(400).send({ 
-    //             success: false,
-    //             message: 'User not found' });
-    //     }
-    //     res.status(200).send({ 
-    //         success: true,
-    //         message: 'User found',
-    //         user });
-    // }
-    // catch (error) {
-    //     console.log('Internal get user error', error);
-    // }
-}
+    try {
+        const user = await userModel.findById({ _id: req.body.id }).select('-password');
+        if (!user) {
+            return res.status(400).send({ 
+                success: false,
+                message: 'User not found' });
+        }
+        res.status(200).send({ 
+            success: true,
+            message: 'User found',
+            user });
+    }
+    catch (error) {
+        console.log('Internal get user error', error);
+    }
+};
 
-module.exports = { getUserController }
+
+
+
+
+
+// UPDATE USER INFO
+const updateUserController = async (req, res) => {
+    try {
+        const { userName, phone, address } = req.body;
+        const user = await userModel.findByIdAndUpdate({ _id: req.body.id }, {
+            userName,
+            phone,
+            address
+        }, { new: true });
+        if (!user) {
+            return res.status(400).send({ 
+                success: false,
+                message: 'User not found' });
+        }
+        res.status(200).send({ 
+            success: true,
+            message: 'User updated successfully', 
+            user });
+    }
+    catch (error) {
+        console.log('Internal update user error', error);
+    }
+};
+
+module.exports = { getUserController, updateUserController };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUserController } = require('../controllers/userControllers');
+const { getUserController, updateUserController } = require('../controllers/userControllers');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -7,6 +7,9 @@ const router = express.Router();
 
 // GET USER
 router.get('/getUser', authMiddleware, getUserController);
+
+// UPDATE USER
+router.put('/updateUser', authMiddleware, updateUserController);
 
 
 module.exports = router; // Export the router
