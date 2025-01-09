3d9c49ab4a1e21df97cf6e846086a4de622cb915
alexG4M3R <alex.372003@gmail.com>
2025-01-09 20:46:58 -0300

    get all food and restaurant id

--- backend/src/controllers/foodControllers.js
+++ backend/src/controllers/foodControllers.js
@@ -1,7 +1,7 @@
 const foodModel = require('../models/foodModel');
 
 // CREATE A NEW FOOD
-const createFood = async (req, res) => {
+const createFoodController = async (req, res) => {
     try {
         const { title, description, price, imageUrl, foodTags, category, code, isAvailable, restaurant } = req.body;
         if (!title || !price || !category) {
@@ -33,6 +33,104 @@ const createFood = async (req, res) => {
     }
 };
 
+
+
+
+
+
+
+
+
+// GET ALL FOODS
+const getAllFoodsController = async (req, res) => {
+    try {
+        const foods = await foodModel.find();
+        if (!foods) {
+            return res.status(404).send({ 
+                success: false,
+                message: 'No food found' 
+            });
+        }
+        res.status(200).send({ 
+            success: true,
+            message: 'Foods retrieved successfully',
+            totalFoods: foods.length,
+            foods
+        });
+    } catch (error) {
+        res.status(400).json({ 
+            success: false,
+            message: 'error in Get All foods',
+            error});
+    }
+};
+
+
+
+
+
+
+
+
+// GET A FOOD BY ID
+const getFoodByIdController = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.params.id);
+        if (!food) {
+            return res.status(404).send({ 
+                success: false,
+                message: 'Food not found' 
+            });
+        }
+        res.status(200).send({ 
+            success: true,
+            message: 'Food by ID successfully',
+            food
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+
+
+
+
+
+
+// GET A FOOD BY RESTAURANT ID
+const getFoodByRestaurantIdController = async (req, res) => {
+    try {
+        const restaurantId = req.params.restaurantId;
+        const foods = await foodModel.find({ restaurant: restaurantId });
+        if (!foods) {
+            return res.status(404).send({ 
+                success: false,
+                message: 'No food found' 
+            });
+        }
+        res.status(200).send({ 
+            success: true,
+            message: 'Foods retrieved successfully',
+            totalFoods: foods.length,
+            foods
+        });
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+
+
+
+
+
+
+// Export functions
 module.exports = {
-    createFood
+    createFoodController,
+    getAllFoodsController,
+    getFoodByIdController,
+    getFoodByRestaurantIdController
 };
--- backend/src/routes/foodRoutes.js
+++ backend/src/routes/foodRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
-const { createFood } = require('../controllers/foodControllers');
+const { createFoodController, getAllFoodsController, getFoodByIdController, getFoodByRestaurantIdController } = require('../controllers/foodControllers');
 const router = express.Router();
 
 // CREATE A NEW FOOD
-router.post('/create', authMiddleware, createFood);
+router.post('/create', authMiddleware, createFoodController);
+
+// GET ALL FOODS
+router.get('/getAll', authMiddleware, getAllFoodsController);
+
+// GET A FOOD BY ID
+router.get('/get/:id', authMiddleware, getFoodByIdController);
+
+// GET A FOOD BY RESTAURANT ID
+router.get('/getByRestaurant/:restaurantId', authMiddleware, getFoodByRestaurantIdController);
 
 
 
