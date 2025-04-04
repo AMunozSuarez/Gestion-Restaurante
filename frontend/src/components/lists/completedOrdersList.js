7350925d6758a9be346de7f2e9fbcd4fbdf5442b
alexG4M3R <alex.372003@gmail.com>
2025-04-03 21:24:15 -0300

    arreglos visuales completed y create

--- frontend/src/components/lists/completedOrdersList.js
+++ frontend/src/components/lists/completedOrdersList.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useParams } from 'react-router-dom'; // Importar useParams para obtener el orderNumber de la URL
 import '../../styles/completedOrderList.css';
 
-const CompletedOrdersList = ({ orders, onSelectOrder, selectedOrderId }) => {
+const CompletedOrdersList = ({ orders, onSelectOrder }) => {
+    const { orderNumber } = useParams(); // Obtener el número de pedido desde la URL
+
     return (
         <div className="completed-orders-list">
             <h3>Pedidos Completados/Cancelados</h3>
+            {/* Encabezado de la lista */}
+            <div className="completed-orders-header">
+                <p>#</p>
+                <p>Cliente</p>
+                <p>Estado</p>
+                <p>Total</p>
+            </div>
             <ul>
                 {orders.map((order) => (
                     <li
                         key={order._id}
-                        className={`${order.status} ${order._id === selectedOrderId ? 'selected' : ''}`}
+                        className={`order-item ${order.status} ${order.orderNumber === parseInt(orderNumber, 10) ? 'selected' : ''}`}
                         onClick={() => onSelectOrder(order)}
                     >
                         <p>#{order.orderNumber}</p>
