63d89ce2be6e9332e9ce243f6d3d856d879bdb52
alexG4M3R <alex.372003@gmail.com>
2025-05-17 16:37:12 -0400

    notification position

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
       <div>
         {/* Configuración del ToastContainer con las opciones especificadas */}
         <ToastContainer
-          position="top-right"
+          position="bottom-right"
           autoClose={5000}
           hideProgressBar={false}
           newestOnTop={false}
@@ -39,7 +39,7 @@ function App() {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="dark"
+          theme="light"
           transition={Flip}
         />
         
